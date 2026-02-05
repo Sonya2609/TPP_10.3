324e54f7559a5c3951603804436a8e832d50e468
Sonya2609 <bondatsm2009@gmail.com>
2026-02-05 20:18:04 +0300

    HW5 added

---
+++ Homeworks/HW5/Program.cs
@@ -0,0 +1,73 @@
+﻿abstract class Character : IDamageable, IHealable
+{
+    protected string Name;
+    protected int Health;
+    public abstract void Attack();
+    public void Move()
+    {
+        Console.WriteLine($"{Name} двигается.");
+    }
+    public void TakeDamage(int damage)
+    {
+        Health -= damage;
+        Console.WriteLine($"{Name} получил {damage} урона. Осталось {Health} здоровья");
+    }
+    public void TakeHeal(int heal)
+    {
+        Health += heal;
+        Console.WriteLine($"{Name} получил {heal} восстановления. Осталось {Health} здоровья");
+    }
+}
+
+class Warrior : Character
+{
+    public Warrior(string name)
+    {
+        this.Name = name;
+        Health = 100;
+    }
+    
+    public override void Attack()
+    {
+        Console.WriteLine($"{Name} с мечом идёт в атаку!");
+    }
+}
+
+class Mage : Character
+{
+    public Mage(string name)
+    {
+        this.Name = name;
+        Health = 100;
+    }
+    
+    public override void Attack()
+    {
+        Console.WriteLine($"{Name} начинает заколдовывать врага!");
+    }
+}
+
+
+interface IDamageable
+{
+    void TakeDamage(int damage);
+}
+
+interface IHealable
+{
+    void TakeHeal(int heal);
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Character[] characters = new Character[2];
+        characters[0] = new Warrior("Вася");
+        characters[1] = new Mage("Петя");
+        foreach (Character character in characters)
+        {
+            character.Attack();
+        }
+    }
+}
